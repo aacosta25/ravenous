16ef99d62dae4059a97efbfe29abc7659e96f28b
Alejandro Acosta <AlejandroAcosta@Macbook.local>
2017-11-07 20:33:21 -0500

    5th commit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,18 +5,70 @@ const sortByOptions = {
 	'Best Match': 'best_match',
 	'Highest Rated': 'rating',
 	'Most Reviewed': 'review_count'
-
 }
 
+
+
 class SearchBar extends React.Component {
-	renderSortByOptions(){
+	constructor (props){
+		super(props);
+
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match'
+		};
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+
+	}
+
+getSortByClass (sortByOption) {
+	if (this.state.sortBy === sortByOption) {
+		return 'active';
+	}else{
+		return '';
+	}
+}
+
+handleSortByChange (sortByOption){
+	this.setState({
+		sortBy: sortByOption
+	});
+}
+
+handleTermChange(event){
+
+	this.setState(
+	{
+		term: event.target.value
+	});
+}
+
+handleLocationChange(event){
+
+	this.setState(
+	{
+		location: event.target.value
+	});
+}
+
+handleSearch(event){
+	this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+	event.preventDefault();
+}
+
+renderSortByOptions(){
 		return Object.keys(sortByOptions).map(sortByOption => {
 			let sortByOptionValue = sortByOptions[sortByOptions];
 
-		return  <li key = {sortByOptionValue}> {sortByOption}</li>;
+		return  <li key = {sortByOptionValue} className = {this.getSortByClass(sortByOptionValue)} onClick = {this.handleSortByChange.bind(this,sortByOptionValue)}> {sortByOption}</li>;
 
 		});
 	}//SortByOptions ends
+
 	render(){
 		return (<div className="SearchBar">
 			  		<div className="SearchBar-sort-options">
@@ -28,12 +80,12 @@ class SearchBar extends React.Component {
 			  		</div>
 		
 			  		<div className="SearchBar-fields">
-				    	<input placeholder="Search Businesses" />
-				    	<input placeholder="Where?" />
+				    	<input placeholder="Search Businesses" onChange = {this.handleTermChange}  />
+				    	<input placeholder="Where?" onChange = {this.handleLocationChange} />
 			  		</div>
 		
 			  		<div className="SearchBar-submit">
-			    		<a>Let's Go</a>
+			    		<a onClick = {this.handleSearch} >Let's Go</a>
 			  		</div>
 				</div>);
 	}//render ends
---
+++ src/util/Yelp.js
@@ -0,0 +1,47 @@
+const clientID = 'Bk9hVETG_R8JS7mJ7R5NFw';
+const secret = 'ZmaSoIn805O38AswxQ5z4UgfkG8u61U0ZDCEBZ12xNKokp5sN4uZLVF7oCO355p2';
+
+let accessToken ;
+
+let Yelp = {
+
+	getAccessToken(){
+		if (accessToken) {
+			return new Promise(resolve => resolve(accessToken));
+		}	
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientID}&client_secret=${secret}`,{method: 'POST'}).then(response => { 
+			return response.json();}).then(jsonResponse => {
+
+				accessToken = jsonResponse.access_token;
+		});		
+	},
+
+	search(term,location,sortBy){
+
+		return Yelp.getAccessToken().then(() => { 
+
+			return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{headers:{Authorization:`Bearer ${accessToken}`}});
+		}).then({method:'POST'}).then(jsonResponse => {
+			if (jsonResponse.businesses) {
+				return jsonResponse.businesses.map(business => {
+
+					 return {
+						id: businesses.id,
+						imageSrc: businesses.imageSrc,
+						name:businesses.name,
+						address:businesses.address,
+						city:businesses.city,
+						state:businesses.state,
+						zipCode:businesses.zipCode,
+						category:businesses.category,
+						rating:businesses.rating,
+						reviewCount:businesses.reviewCount
+					}
+				});
+			}
+		})
+	}
+
+};// Yelp object ends
+
+export default Yelp;
